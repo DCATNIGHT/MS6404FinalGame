84d993189da2bd4b81ac883cbdadfbbd22c7a2ad
Unknown <assisiwaterman@hotmail.co.uk>
2017-11-23 12:05:48 +0000

    Unity project added

---
+++ MS6404_Final_Game/Assets/Fungus_2D_Pack/Scripts/OK_TriggerConversation.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+//using UnityStandardAssets.Characters.FirstPerson;
+
+public class OK_TriggerConversation : MonoBehaviour
+{
+
+    // Variables
+
+    // These private variables are set within the start and are used to control the player character when they enter the trigger box.
+    private GameObject GO_Player;
+    public bool bl_Toggle;
+    private Rigidbody2D rb_player;
+
+    // This public variable is for setting the flowchart. You should be able to set the flowchart per trigger in the editor.
+    public Flowchart fc_NPCDialog;
+    public string st_MessageReciever;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Game Objects and components are found so that we can reference them later in the script.
+        GO_Player = GameObject.FindGameObjectWithTag("Player");
+        rb_player = GO_Player.GetComponent<Rigidbody2D>();
+
+        // We need to set the image and flowchart to false so that we can activate them when the player enters the trigger.
+
+        fc_NPCDialog.gameObject.SetActive(false);
+
+        bl_Toggle = false;
+
+
+    }
+
+    //This void 
+    private void OnTriggerStay2D(Collider2D cl_trigger)
+    {
+        if (cl_trigger.gameObject.tag == "Player")
+        {
+
+            if (Input.GetKeyDown(KeyCode.E) && bl_Toggle == false)
+            {
+                fc_NPCDialog.gameObject.SetActive(true);
+                fc_NPCDialog.SendFungusMessage(st_MessageReciever);
+                rb_player.isKinematic = true;
+                bl_Toggle = true;
+
+            }
+            else if (Input.GetKeyDown(KeyCode.E) && bl_Toggle == true)
+            {
+                rb_player.isKinematic = false;
+                fc_NPCDialog.gameObject.SetActive(false);
+                bl_Toggle = false;
+            }
+        }
+    }
+
+    public void RBEnable()
+    {
+
+        rb_player.isKinematic = false;
+    }
+
+
+}
---
+++ MS6404_Final_Game/Assets/Fungus_2D_Pack/Scripts/Screen_Wrap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screen_Wrap : MonoBehaviour {
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var cam = Camera.main;
+
+        var viewportPosition = cam.WorldToViewportPoint(transform.position);
+
+        var newPosition = transform.position;
+
+        if (viewportPosition.x > 1 || viewportPosition.x < 0)
+        {
+            newPosition.y = -newPosition.y;
+        }
+
+        if (viewportPosition.y > 1 || viewportPosition.y < 0)
+        {
+            newPosition.y = -newPosition.y;
+        }
+
+        transform.position = newPosition;
+    }
+}
